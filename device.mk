dec16959a734661bd467e00476335cdd03261846
kmiit <2421768138@qq.com>
2023-08-30 13:39:57 +0800

    socrates: Fix Camera

--- device.mk
+++ device.mk
@@ -396,8 +396,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
  PRODUCT_PACKAGES += \
-     sensors.xiaomi \
-     sensors.udfps
+     sensors.xiaomi 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
