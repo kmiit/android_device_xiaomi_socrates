f1f3d7681c2fbed2e0df38580b5fdcc664f2447c
kmiit <2421768138@qq.com>
2023-09-16 13:51:07 +0800

    Fix camera

--- device.mk
+++ device.mk
@@ -434,7 +434,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
  PRODUCT_PACKAGES += \
-     sensors.xiaomi 
+     sensors.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
