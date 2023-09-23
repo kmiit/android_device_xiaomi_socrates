d18e87c3092f29244b96d3f9ac4a67ab3b6f2b29
kmiit <2421768138@qq.com>
2023-09-24 00:15:07 +0800

    socrates: Drop vendor.dumpstate-xiaomi and spu service 01-21 14:35:48.738     1     1 E init    : Could not start service 'vendor.dumpstate-xiaomi' as part of class 'hal': File /vendor/bin/hw/android.hardware.dumpstate-service.xiaomi (labeled "u:object_r:vendor_file:s0") has incorrect label or no domain transition from u:r:init:s0 to another SELinux domain defined. Have you configured your service correctly? https://source.android.com/security/selinux/device-policy#label_new_services_and_address_denials 01-21 14:35:48.738     1     1 E init    : Could not start service 'spuservice' as part of class 'hal': Cannot find '/vendor/bin/hw/vendor.qti.spu@2.0-service': No such file or directory 01-21 14:35:48.739     1     1 E init    : Could not start service 'spu_service' as part of class 'hal': Cannot find '/vendor/bin/hw/vendor.qti.spu@1.1-service': No such file or directory

--- device.mk
+++ device.mk
@@ -192,12 +192,12 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     libdrm.vendor
 
-# DumpState
-PRODUCT_PACKAGES += \
-    android.hardware.dumpstate@1.0.vendor \
-    android.hardware.dumpstate@1.1.vendor \
-    android.hardware.dumpstate-V1-ndk \
-    libdumpstateutil
+# # DumpState
+# PRODUCT_PACKAGES += \
+#     android.hardware.dumpstate@1.0.vendor \
+#     android.hardware.dumpstate@1.1.vendor \
+#     android.hardware.dumpstate-V1-ndk \
+#     libdumpstateutil
 
 # Fastbootd
 PRODUCT_PACKAGES += \
