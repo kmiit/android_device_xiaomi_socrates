ed8d2309d0baaf76b05f78bfabbc0e7866f02240
kmiit <2421768138@qq.com>
2023-10-13 17:01:25 +0800

    add light service

--- device.mk
+++ device.mk
@@ -300,12 +300,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.xml \
     android.hardware.security.keymint-V1-ndk.vendor \
+    android.hardware.security.keymint-V2-ndk.vendor \
     android.hardware.security.secureclock-V1-ndk.vendor \
     android.hardware.security.sharedsecret-V1-ndk.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
     
 # Media
 PRODUCT_PACKAGES += \
