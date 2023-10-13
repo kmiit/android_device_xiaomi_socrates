2348f950db22e9f6a582cd0532ae6f895e2195f2
kmiit <2421768138@qq.com>
2023-10-13 21:17:02 +0800

    socrates: Move android.hardware.security.rkp-V1-ndk to vendor

--- device.mk
+++ device.mk
@@ -298,7 +298,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint-V1-ndk.vendor \
     android.hardware.security.keymint-V2-ndk.vendor \
-    android.hardware.security.rkp-V1-ndk \
+    android.hardware.security.rkp-V1-ndk.vendor \
     android.hardware.security.secureclock-V1-ndk.vendor \
     android.hardware.security.sharedsecret-V1-ndk.vendor
 
