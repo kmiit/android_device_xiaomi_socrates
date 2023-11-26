b97c0d8cb380f227af0258536194732359cd4d39
kmiit <2421768138@qq.com>
2023-11-26 23:37:27 +0800

    Update

--- device.mk
+++ device.mk
@@ -485,7 +485,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge \
     sensors.xiaomi
 
